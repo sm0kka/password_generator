92850183289be876fbb20a6800ae169f591c4f0a
sm0kka <sm0kka@gmail.com>
2022-08-23 22:26:09 +0300

    Rewrite code

--- pg.py
+++ pg.py
@@ -1,127 +1,177 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 import argparse
+from dataclasses import dataclass, asdict
 from secrets import choice
-import string
-from functools import lru_cache
-import logging
+from string import digits, ascii_uppercase, ascii_lowercase
 
+VERSION = '0.1.5'
 
-VERSION = '0.1.4'  # Last edit 10.04.2021
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-OPTION = {
-    'd': string.digits,
-    'l': string.ascii_lowercase,
-    'u': string.ascii_uppercase,
-    'p': string.punctuation,
-    'v': 'aeiou'  # vowels
+CHAR_SETS = {
+    'd': digits,
+    'l': ascii_lowercase,
+    'u': ascii_uppercase,
+    'p': '!$&(*.,+)-@^_?',
+    'v': 'aeiou',  # vowels
+    't': '!/-?'  # token punctuations
 }
 
 
-class PasswordGenerator:
-    def __init__(self, **kwargs):
-        """kwargs: chars, amount, manual, set_chars, verbose, temporary, version"""
-        self.passwords = set()
-        self._kwargs = kwargs
+def singleton(class_):
+    instances = {}
 
-    @property
-    def kwargs(self):
-        return self._kwargs
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
 
-    def temp_pass(self):
-        # password_collector: tuple of tuples (int: amount, str: set_chars)
-        password_collector = (
-            (1, OPTION['u']),
-            (1, OPTION['v']),
-            (1, OPTION['l']),
-            (5, OPTION['d'])
-        )
+    return get_instance
 
-        for _ in range(self.kwargs['amount']):
-            generate = ''.join(
-                ''.join(
-                    choice(set_chars) for _ in range(number_of_chars)
-                )
-                for number_of_chars, set_chars in password_collector
-            )
-            self.passwords.add(''.join(generate))
-        if self.kwargs['verbose']:
-            logging.info('Generate temporary password like: Gik01103')
 
-        return self.passwords
+@dataclass
+class Password:
+    value: str = ''
 
-    @lru_cache()
-    def get_password(self):
-        if self.kwargs['version']:
-            logging.info(f'Password Generator {VERSION}')
-            return
+    def __repr__(self):
+        return f"{self.value}"
 
-        if self.kwargs['temporary']:
-            return PasswordGenerator.temp_pass(self)
 
-        if all(False for i in self.kwargs['set_chars'] if i in OPTION):
-            set_chars = self.kwargs['set_chars']
-            logging.error(f'ValueError: unknown parameters "{set_chars}" in option -s'
-                          f'\nRun with default charset: "dlu"')
-            # Default set of chars letters and digits
-            self.kwargs['set_chars'] = 'dlu'
+@dataclass
+class Options:
+    char_set: str = 'dul'
+    amount: int = 1
+    lenght: int = 14
+    temporary: bool = False
+    version: bool = False
 
-        # Generate chars string
-        chars_str = ''.join(OPTION.get(i, '')
-                            for i in self.kwargs['set_chars'] if i in OPTION)
+    def __repr__(self):
+        return f'{self.char_set=} {self.amount=} ' \
+               f'{self.lenght=} ' \
+               f'{self.temporary=} {self.version=}'
 
-        if self.kwargs['manual'] in range(1, len(chars_str)):
-            chars_str = chars_str[:self.kwargs['manual']]  # Slice set
+    def __add__(self, other):
+        res = {**self.__dict__, **other.__dict__}
+        return Options(**res)
 
-        if self.kwargs['verbose']:
-            logging.info(f'Current set({len(chars_str)}): {chars_str}')
 
-        for _ in range(self.kwargs['amount']):
-            self.passwords.add(
-                ''.join(
-                    choice(chars_str) for _ in range(self.kwargs['chars'])
-                )
-            )
-        return self.passwords
+@singleton
+class Generator:
+    def __init__(self):
+        self.options = Options()
 
-    def generated_passwords(self):
-        if self.kwargs['amount'] > 1:
-            result = [
-                f'\t{index}\t{password}\n'
-                for index, password in enumerate(self.passwords, 1)
-            ]
-            return ''.join(result)
+    def set_opts(self, options: Options | dict) -> None:
+        res = {**self.options.__dict__, **self.set_options(options).__dict__}
+        self.options = Options(**res)
 
-        return '\t\t' + ''.join(self.passwords)
+    def get_passwd(self) -> list[Password()]:
+        gen_function = self.temporary_passwd if self.options.temporary else self._generate_passwd
+
+        if self.options.amount >= 1:
+            return [gen_function() for _ in range(self.options.amount)]
+        else:
+            print('Amount could be >= 1')
+
+    def _generate_passwd(self) -> Password():
+        char_set = ''.join([
+            CHAR_SETS.get(option) for option in self.options.char_set
+            if option in CHAR_SETS
+        ])
+        lenght = self.options.lenght
+        password = [choice(char_set) for _ in range(lenght)]
+        return Password(''.join(password))
+
+    def set_options(self, options: Options | dict) -> Options:
+        so = self.options
+        if isinstance(options, Options):
+            return so + options
+        elif isinstance(options, dict):
+            tmp = so.__dict__ | options
+            return Options(**tmp)
+        else:
+            raise ValueError('Invalid options')
+
+    @staticmethod
+    def temporary_passwd() -> Password:
+        password_collector = (1, 'u'), (1, 'v'), (1, 'l'), (5, 'd')
+        res = []
+        for amount, char_set in password_collector:
+            for _ in range(amount):
+                res.append(choice(CHAR_SETS[char_set]))
+        password = ''.join(res)
+        return Password(password)
+
+
+def print_pass(passwords: list[Password]) -> None:
+    if passwords:
+        for i, password in enumerate(passwords, 1):
+            print(f'{i}\t{password}')
+
+
+def main() -> None:
+    opts = Options(**vars(cli_parser()))
+
+    if opts.version:
+        return print(f'Password Generator ver.{VERSION}')
+
+    passwd = Generator()
+    # ss = Options(amount=13, char_set='dt', lenght=128)
+    # s = passwd.options
+    # z = s + ss
+    # print(z)
+    # passwd.set_opts(z)
+    passwd.set_opts(opts)
+
+    print_pass(passwd.get_passwd())
+    return
 
 
 def cli_parser():
-    parser = argparse.ArgumentParser(prog='pg.py', description='Password generator',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-c', '--chars', metavar='', type=int, default=14,
-                        help='Length of password in characters')
-    parser.add_argument('-a', '--amount', metavar='', type=int, default=1,
-                        help='Amount of passwords')
-    parser.add_argument('-m', '--manual', metavar='', type=int, default=0,
-                        help='Manual slice set of chars')
-    parser.add_argument('-s', '--set_chars', type=str, default='dlu',
-                        help=f'Charset for password generation'
-                             f'(d: digits, l: lowercase letters, u: uppercase letters, p: punctuation)')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Show set of chars before generation')
-    parser.add_argument('-V', '--version', action='store_true',
-                        help='Show version')
-    parser.add_argument('-t', '--temporary', action='store_true',
-                        help='Generate temporary password. Ignore all other settings except of -a. Example: Gik01103')
+    parser = argparse.ArgumentParser(
+        prog='pg.py', description='Password generator',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        '-l',
+        '--lenght',
+        metavar='',
+        type=int,
+        default=14,
+        help='Length of password in characters'
+    )
+    parser.add_argument(
+        '-a',
+        '--amount',
+        metavar='',
+        type=int,
+        default=1,
+        help='Amount of passwords'
+    )
+    # parser.add_argument('-m', '--manual', metavar='', type=int, default=0,
+    #                     help='Manual slice set of chars')
+    parser.add_argument(
+        '-c',
+        '--char_set',
+        type=str,
+        default='dlu',
+        help='Charset for password generation(d: digits, l: lowercase letters, u: uppercase letters, p: punctuation)',
+    )
+    # parser.add_argument('-v', '--verbose', action='store_true',
+    #                     help='Show set of chars before generation')
+    parser.add_argument(
+        '-V',
+        '--version',
+        action='store_true',
+        help='Show version'
+    )
+    parser.add_argument(
+        '-t',
+        '--temporary',
+        action='store_true',
+        help='Generate temporary password. Ignore all other settings except of -a. Example: Gik01103'
+    )
 
     return parser.parse_args()
 
 
-@lru_cache()
-def main():
-    password = PasswordGenerator(**vars(cli_parser()))
-    password.get_password()
-    return password.generated_passwords()
-
-
 if __name__ == '__main__':
-    print(main())
+    main()
